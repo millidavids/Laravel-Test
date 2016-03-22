11b34213fd0c6b60eef429daecbddbaf9738ac72
millidavids <david.yurek@realpage.com>
2016-03-22 11:23:02 -0400

    view layout structure

--- packages/millidavids/SimpleGui/src/resources/views/index.blade.php
+++ packages/millidavids/SimpleGui/src/resources/views/index.blade.php
@@ -1,13 +1,17 @@
-{!! millidavids\SimpleGui\ViewRenderer::table() !!}
-<hr>
-<br>
-{!! millidavids\SimpleGui\ViewRenderer::dropdown() !!}
-<hr>
-<br>
-{!! millidavids\SimpleGui\ViewRenderer::link() !!}
-<hr>
-<br>
-{!! millidavids\SimpleGui\ViewRenderer::textfield() !!}
-<hr>
-<br>
-{!! millidavids\SimpleGui\ViewRenderer::label() !!}
+@extends('SimpleGui::layouts.package')
+
+@section('content')
+    {{--{!! millidavids\SimpleGui\ViewRenderer::table() !!}--}}
+    {{--<hr>--}}
+    {{--<br>--}}
+    {{--{!! millidavids\SimpleGui\ViewRenderer::dropdown() !!}--}}
+    {{--<hr>--}}
+    {{--<br>--}}
+    {{--{!! millidavids\SimpleGui\ViewRenderer::link() !!}--}}
+    {{--<hr>--}}
+    {{--<br>--}}
+    {{--{!! millidavids\SimpleGui\ViewRenderer::textfield() !!}--}}
+    {{--<hr>--}}
+    {{--<br>--}}
+    {{--{!! millidavids\SimpleGui\ViewRenderer::label() !!}--}}
+@endsection
